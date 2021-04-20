cd021678dc53d9b8c0a77e251f5d858a0ec08a45
ddl-viniatska <69326214+ddl-viniatska@users.noreply.github.com>
2021-04-20 14:50:05 -0700

    Create git_rw_dataset_diabetes.R

---
+++ git_rw_dataset_diabetes.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+dir_local_dataset = '/mnt/data/Git-RW'
+dir_fullpath_file = file.path(dir_local_dataset, "diabetes.csv")
+
+df_diabetes = read.csv(dir_fullpath_file)
+
+target = "Outcome"
+df_diabetes_mean_features = as.data.frame.list(colMeans(df_diabetes)) %>% select(-Outcome)
+
+df_diabetes$Outcome = as.factor(df_diabetes$Outcome)
+
+model <- glm(Outcome ~ ., data=df_diabetes, family=binomial)
+
+#predict(model, newdata=df_diabetes_mean_features, type="response")
+
+saveRDS(model, file.path(dir_local_dataset, "model.rds"))
+write.csv(df_diabetes_mean_features, file.path(dir_local_dataset, "diabetes_mean.csv"), row.names=FALSE)
