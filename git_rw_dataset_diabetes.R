9063ecff22eaf1dcfe1edabf27afc7ed73d29ad1
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2021-05-09 14:25:16 -0700

    Pushed from Domino: /u/integration-test/GB-RW/runs/6098510046d99b11d65ee61c

--- git_rw_dataset_diabetes.R
+++ git_rw_dataset_diabetes.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 
-dir_local_dataset = '/mnt/data/Git-RW'
-dir_fullpath_file = file.path(dir_local_dataset, "diabetes.csv")
+dir_write_dataset = '/mnt/data/GB-RW'
+dir_read_dataset = '/mnt/data/snapshots/GB-RW/diabetes'
+dir_fullpath_file = file.path(dir_read_dataset, "diabetes.csv")
+
 
 df_diabetes = read.csv(dir_fullpath_file)
 
@@ -14,5 +16,5 @@ model <- glm(Outcome ~ ., data=df_diabetes, family=binomial)
 
 #predict(model, newdata=df_diabetes_mean_features, type="response")
 
-saveRDS(model, file.path(dir_local_dataset, "model.rds"))
-write.csv(df_diabetes_mean_features, file.path(dir_local_dataset, "diabetes_mean.csv"), row.names=FALSE)
+saveRDS(model, file.path(dir_write_dataset, "model.rds"))
+write.csv(df_diabetes_mean_features, file.path(dir_write_dataset, "diabetes_mean.csv"), row.names=FALSE)
